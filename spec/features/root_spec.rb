6038fa03e1ef130d87488b2d0d30f44e5c3f4f65
Stacy Andrews <stacy@cribber.com>
2018-03-17 17:44:22 +0800

    minor fixes

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "App Root" do
   scenario "Traveler goes to the root url it redirects to the policy price creation" do
     visit "/"
 
-    expect(page).to have_current_path(new_policy_price_path, only_path: true)
+    expect(page).to have_current_path(new_policy_price_path, ignore_query: true)
   end
 end
