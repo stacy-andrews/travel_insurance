2526eab8c2d978922b3b3ee22b8e77de59513730
Stacy Andrews <stacy@cribber.com>
2018-03-20 08:46:26 +0800

    cleaning up form validation specs

--- spec/forms/policy_price_form_spec.rb
+++ spec/forms/policy_price_form_spec.rb
@@ -1,75 +1,60 @@
 require "rails_helper"
 
 describe PolicyPriceForm do
+  def is_valid(param)
+    form = PolicyPriceForm.new(param)
+    form.valid?
+
+    expect(form.errors[param.keys.first].size).to be(0)
+  end
+
+  def is_not_valid(param)
+    form = PolicyPriceForm.new(param)
+    expect(form).to_not be_valid
+
+    expect(form.errors[param.keys.first].size).to be(1)
+  end
+
   it "is valid when the users age is between 18 and 69" do
     travel_to(Time.zone.local(2018, 3, 19)) do
-      (18..69).each do |age| 
-        form = PolicyPriceForm.new(date_of_birth: (age.years.ago).to_s)
-        form.valid?
-
-        expect(form.errors[:date_of_birth].size).to be(0)
-      end
+      (18..69).each { |age| is_valid(date_of_birth: age.years.ago.to_s) }
     end
   end
 
   it "is not valid when the users age is less than 18" do
-    (1..17).each do |age|
-      form = PolicyPriceForm.new(date_of_birth: age.years.ago.to_s)
-      expect(form).to_not be_valid
-
-      expect(form.errors[:date_of_birth].size).to be(1)
+    travel_to(Time.zone.local(2018, 3, 19)) do
+      (1..17).each { |age| is_not_valid(date_of_birth: age.years.ago.to_s) }
     end
   end
 
   it "is not valid when the users age is more than 69" do
-    (70..100).each do |age|
-      form = PolicyPriceForm.new(date_of_birth: age.years.ago.to_s)
-      expect(form).to_not be_valid
-
-      expect(form.errors[:date_of_birth].size).to be(1)
+    travel_to(Time.zone.local(2018, 3, 19)) do
+      (70..100).each  { |age| is_not_valid(date_of_birth: age.years.ago.to_s) }
     end
   end
 
   it "is not valid when the date of birth is not a valid iso date" do
-    form = PolicyPriceForm.new(date_of_birth: 'a')
-    expect(form).to_not be_valid
-
-    expect(form.errors[:date_of_birth].size).to be(1)
+    is_not_valid(date_of_birth: 'a')
   end
 
   it "is not valid when the date of birth is missing" do
-    form = PolicyPriceForm.new(date_of_birth: nil)
-    expect(form).to_not be_valid
-
-    expect(form.errors[:date_of_birth].size).to be(1)
+    is_not_valid(date_of_birth: nil)
   end
 
   it "is not valid when the departure date is not a date" do
-    form = PolicyPriceForm.new(departure_date: 'a')
-    expect(form).to_not be_valid
-
-    expect(form.errors[:departure_date].size).to be(1)
+    is_not_valid(departure_date: 'a')
   end
 
   it "is not valid when the departure date is missing" do
-    form = PolicyPriceForm.new(departure_date: nil)
-    expect(form).to_not be_valid
-
-    expect(form.errors[:departure_date].size).to be(1)
+    is_not_valid(departure_date: nil)
   end
 
   it "is not valid when the return date is not a date" do
-    form = PolicyPriceForm.new(return_date: 'a')
-    expect(form).to_not be_valid
-
-    expect(form.errors[:return_date].size).to be(1)
+    is_not_valid(return_date: 'a')
   end
 
   it "is not valid when the return date is missing" do
-    form = PolicyPriceForm.new(return_date: nil)
-    expect(form).to_not be_valid
-
-    expect(form.errors[:return_date].size).to be(1)
+    is_not_valid(return_date: nil)
   end
 
   it "is not valid when the departure date occurs after the return date" do
@@ -83,19 +68,13 @@ describe PolicyPriceForm do
 
   it "is not valid when the departure date occurs before the current date" do
     travel_to(Time.zone.local(2018, 3, 19)) do
-      form = PolicyPriceForm.new(departure_date: '2018-02-19')
-      expect(form).to_not be_valid
-
-      expect(form.errors[:departure_date].size).to be(1)
+      is_not_valid(departure_date: '2018-02-19')
     end
   end
 
   it 'is valid when the departure date occurs today or in the future' do
     travel_to(Time.zone.local(2018, 3, 19)) do
-      form = PolicyPriceForm.new(departure_date: '2018-03-19')
-      form.valid?
-
-      expect(form.errors[:departure_date].size).to be(0)
+      is_not_valid(departure_date: '2018-03-19')
     end
   end
 
